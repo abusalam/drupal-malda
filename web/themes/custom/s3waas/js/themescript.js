268b245629dd40174af9bf1851607e7276615aba
Sumanta Kabiraj <sumantablog@gmail.com>
2019-12-02 18:26:30 +0530

    Add Back To Top Button With JavaScript

--- web/themes/custom/s3waas/js/themescript.js
+++ web/themes/custom/s3waas/js/themescript.js
@@ -1,6 +1,20 @@
 $("ul.menu").first().wrap('<div id="cssmenu"></div>');
+$('.region-topbar').append('<a href="#" title="sroll" class="scrollToTop" style="display: none;"><i class="fa fa-angle-up"></i></a>');
 
 $("#cssmenu").menumaker({
   title: "Menu",
   format: "multitoggle"
+});
+
+$(window).scroll(function() {
+  if ($(this).scrollTop()) {
+    $('.scrollToTop').fadeIn();
+  } else {
+    $('.scrollToTop').fadeOut();
+  }
+});
+
+$('.scrollToTop').on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: 0 }, '300');
 });
