5afac093ef59e4dc5704cd3c513673d820591dad
Sumanta Kabiraj <sumantablog@gmail.com>
2019-12-02 19:28:15 +0530

    Add Footer background Image

--- web/themes/custom/s3waas/js/themescript.js
+++ web/themes/custom/s3waas/js/themescript.js
@@ -1,5 +1,5 @@
 $("ul.menu").first().wrap('<div id="cssmenu"></div>');
-$('.region-topbar').append('<a href="#" title="sroll" class="scrollToTop" style="display: none;"><i class="fa fa-angle-up"></i></a>');
+$('.region-topbar').append('<a href="#" title="Back To Top" class="scrollToTop" style="display: none;"><i class="fa fa-angle-up"></i></a>');
 
 $("#cssmenu").menumaker({
   title: "Menu",
@@ -18,3 +18,5 @@ $('.scrollToTop').on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({ scrollTop: 0 }, '300');
 });
+
+$('.footer').before(' <img src="https://sumantablog.github.io/drupal-malda/static/images/footer_top_bg.gif" alt="Biswa Bangla" width="100%">');
