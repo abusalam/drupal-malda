f8afe24ef844f0be0b95a27598a701acfc71a65d
Sumanta Kabiraj <sumantablog@gmail.com>
2019-12-03 11:19:01 +0530

    Fixed Home Slider

--- web/themes/custom/s3waas/js/themescript.js
+++ web/themes/custom/s3waas/js/themescript.js
@@ -1,13 +1,13 @@
-(function ($, Drupal) {
+(function($, Drupal) {
   Drupal.behaviors.myModuleBehavior = {
-    attach: function (context, settings) {
+    attach: function(context, settings) {
       $("ul.menu").first().once().wrap('<div id="cssmenu"></div>');
-      
+
       $('.region-topbar').append('<a href="#" title="Back To Top" class="scrollToTop" style="display: none;"><i class="fa fa-angle-up"></i></a>');
 
-      $("#cssmenu").menumaker({title: "Menu", format: "multitoggle"});
+      $("#cssmenu").menumaker({ title: "Menu", format: "multitoggle" });
 
-      $(window).scroll(function () {
+      $(window).scroll(function() {
         if ($(this).scrollTop()) {
           $('.scrollToTop').fadeIn();
         } else {
@@ -15,7 +15,7 @@
         }
       });
 
-      $('.scrollToTop').on('click', function (e) {
+      $('.scrollToTop').on('click', function(e) {
         e.preventDefault();
         $('html, body').animate({
           scrollTop: 0
@@ -30,14 +30,14 @@
         pauseText: "<span class='hide'>Pause</span>",
         playText: "<span class='hide'>Play</span>",
         controlNav: false,
-        start: function (slider) {
-            $('body').find('.flexslider').resize();
-            if (slider.count == 1) {
-                slider.pausePlay.parent().remove();
-            }
+        start: function(slider) {
+          $('body').find('.flexslider').resize();
+          if (slider.count == 1) {
+            slider.pausePlay.parent().remove();
+          }
         }
       });
-      $('.footer').once().before('<img src="https://sumantablog.github.io/drupal-malda/static/images/footer_top_bg.gif" alt="Biswa Bangla" width="100%">');
+      $('.footer').once().before('<img src="/themes/custom/s3waas/images/footer-background.gif" alt="Biswa Bangla" width="100%">');
     }
   };
 })(jQuery, Drupal);
