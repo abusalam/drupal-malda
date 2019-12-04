18e1b83ab69e178165f5b26f1dc8d9e4d52b0d33
Sumanta Kabiraj <sumantablog@gmail.com>
2019-12-04 17:49:17 +0530

    Set Theme Style Using Cookie (Testing)

--- web/themes/custom/s3waas/js/themescript.js
+++ web/themes/custom/s3waas/js/themescript.js
@@ -25,14 +25,46 @@
 
       $('.contrast').click(function() {
         var code = this.id;
+        setStyle(code);
+      });
+
+      var style = getCookie("style");
+      setStyle(style);
+
+      function setStyle(code) {
         if (code == 'dark') {
+          setCookie("style", code, 30);
           $('head').append('<link href="/themes/custom/s3waas/css/base/contrast.css" rel="stylesheet" id="newcss" title="contrast" />');
         } else if (code == 'light') {
+          setCookie("style", code, 30);
           $('link[href="/themes/custom/s3waas/css/base/contrast.css"]').remove();
         } else {
           return false;
         }
-      });
+      }
+
+      function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toGMTString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
+
+      function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+      }
 
       $('.font-size').click(function() {
         var code = this.id;
