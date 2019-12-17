9653204e0fb06ca40e810c3e5bb5c4c7349d30bd
Sumanta Kabiraj <sumantablog@gmail.com>
2019-12-17 19:17:29 +0530

    Fixed Footer Slider

--- web/themes/custom/s3waas/js/slider.js
+++ web/themes/custom/s3waas/js/slider.js
@@ -1,6 +1,6 @@
-(function ($, Drupal) {
+(function($, Drupal) {
   Drupal.behaviors.slider = {
-    attach: function (context, settings) {
+    attach: function(context, settings) {
       $('.home-slider', context).flexslider({
         animation: ($('body').hasClass('rtl')) ? "fade" : "slide",
         directionNav: true,
@@ -10,12 +10,28 @@
         pauseText: "<span class='hide'>Pause</span>",
         playText: "<span class='hide'>Play</span>",
         controlNav: false,
-        start: function (slider) {
+        start: function(slider) {
           $('body').find('.flexslider').resize();
           if (slider.count == 1) {
             slider.pausePlay.parent().remove();
           }
         }
+      });
+      jQuery(document).ready(function() {
+        jQuery("#footerScrollbar").flexslider({
+          animation: "slide",
+          animationLoop: true,
+          itemWidth: 300,
+          minItems: 2,
+          slideshow: 1,
+          move: 1,
+          controlNav: false,
+          pausePlay: true,
+          prevText: "<span class='hide'>Previous</span>",
+          nextText: "<span class='hide'>Next</span>",
+          pauseText: "<span class='hide'>Pause</span>",
+          playText: "<span class='hide'>Play</span>",
+        })
       });
     }
   };
